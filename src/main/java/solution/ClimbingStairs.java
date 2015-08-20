195a44752ad21fbc7ab06a8ac95cb87c1d0de84f
wzktravel <wzktravel@gmail.com>
2015-08-20 14:51:55 +0800

    Climbing Stairs

--- src/main/java/solution/ClimbingStairs.java
+++ src/main/java/solution/ClimbingStairs.java
@@ -6,7 +6,28 @@ package solution;
 public class ClimbingStairs {
 
     public int climbStairs(int n) {
-        return 0;
+        if (n <= 2) {
+            return n;
+        }
+
+        int pre = 1;
+        int change = 1;
+        int res = 1;
+
+        for (int i = 2; i <= n; i++) {
+            change = res;
+            res = res + pre;
+            pre = change;
+        }
+
+        return res;
+    }
+
+    public int climbStairs2(int n) {
+        if (n <= 2) {
+            return n;
+        }
+        return climbStairs2(n - 1) + climbStairs2(n - 2);
     }
 
 }
---
+++ src/test/java/solution/ClimbingStairsTest.java
@@ -0,0 +1,16 @@
+package solution;
+
+import org.junit.Test;
+
+/**
+ * Created by wangzhengkun on 2015/8/20.
+ */
+public class ClimbingStairsTest {
+
+    @Test
+    public void test() {
+        ClimbingStairs cs = new ClimbingStairs();
+        System.out.println(cs.climbStairs(4));
+    }
+
+}
