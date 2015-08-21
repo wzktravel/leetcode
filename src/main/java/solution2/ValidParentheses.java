917517c7752e48a703c952a09de1e678edcc5cc4
wzktravel <wzktravel@gmail.com>
2015-08-21 19:37:10 +0800

    valid parentheses

---
+++ src/main/java/solution2/ValidParentheses.java
@@ -0,0 +1,16 @@
+package solution2;
+
+/**
+ * Created by wangzhengkun on 2015/8/21.
+ */
+public class ValidParentheses {
+
+    public boolean isValid(String s) {
+        boolean isValid = true;
+
+
+
+        return isValid;
+    }
+
+}
